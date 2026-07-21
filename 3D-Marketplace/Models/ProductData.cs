3a4d279bfc26cce8fbd9a152849b1b343514733d
silent <killisgo083@gmail.com>
2026-07-21 04:29:47 +0300

    Add EF Core, 3D model viewer, and UI enhancements
    Added Entity Framework Core and SQL Server support in the project. Introduced `ProductData` class for managing product details with database schema mapping. Enhanced `Index.cshtml` with improved product input fields and a warning for 3D model uploads.
    
    Implemented a 3D model viewer using Three.js in `3dView.cshtml` and `3dView.js`, supporting model rendering, interaction, and responsive design. Updated `_Layout.cshtml` to include an avatar icon.
    
    Added binary files for 3D models and textures. Updated `X.svg` and added `avatar.svg` for improved UI visuals.

---
+++ 3D-Marketplace/Models/ProductData.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace _3D_Marketplace.Models {
+    public class ProductData {
+        public int Id { get; set; }
+
+        [Column("varchar(100)")]
+        public string ViewDefaultRotation { get; set; }
+
+        public float CameraDefaultZPos { get; set; }
+
+        [Column("varchar(500)")]
+        public string? BaseColor { get; set; }
+
+        [Column("varchar(500)")]
+        public string? Roughness { get; set; }
+
+        [Column("varchar(500)")]
+        public string? Metallic { get; set; }
+
+        [Column("varchar(500)")]
+        public string? NormalMap { get; set; }
+
+        [Column("varchar(500)")]
+        public string? Emission { get; set; }
+        public float Emission_Brightness { get; set; }
+
+        [Column("char(7)")]
+        public string Emission_Color { get; set; } = "#000000"; // default to black
+
+        [Column("varchar(500)")]
+        public string? AmbientOcclusion { get; set; }
+
+        [Column("varchar(500)")]
+        public string? HDRI { get; set; }
+        public bool HDRI_ShowAsBackground {  get; set; }
+        public float HDRI_Brightness{ get; set; }
+
+        [Required]
+        [Column("varchar(300)")]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+        [Column("varchar(3500)")]
+        public string Description { get; set; }
+
+        // UserData: seller
+    }
+}
---
+++ 3D-Marketplace/wwwroot/js/3dView.js
@@ -0,0 +1,67 @@
+﻿import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
+
+const scene = new THREE.Scene(); // Set up the scene
+
+const canvas = document.getElementById('canvas');
+const camera = new THREE.PerspectiveCamera(45, canvas.clientWidth / canvas.clientHeight, 0.1, 100);
+
+const cameraParent = new THREE.Object3D();
+cameraParent.add(camera);
+scene.add(cameraParent);
+
+const render = new THREE.WebGLRenderer({ antialias: true, alpha: true, preserveDrawingBuffer: true });
+
+let model = new THREE.Object3D();
+model = null;
+
+render.setSize(canvas.clientWidth, canvas.clientHeight);
+canvas.appendChild(render.domElement);
+
+
+const Timer = new THREE.Timer();
+Timer.connect(document);
+function animate(timestamp) {
+    requestAnimationFrame(animate);
+
+    Timer.update(timestamp);
+
+    render.render(scene, camera);
+}
+
+function setUp() {
+    // here get the model path and the texture and all the nedded data
+    animate();
+}
+setUp();
+
+canvas.addEventListener('wheel', (e) => {
+    camera.position.z += e.deltaY * Timer.getDelta();
+    if (camera.position.z < 2) camera.position.z = 1;
+});
+canvas.addEventListener('mousemove', (e) => {
+    if (e.buttons === 2) { // if right click
+        if (!model) return;
+
+        canvas.style.cursor = "url('/resources/cursorRotate.svg'), auto"
+        model.rotation.z += e.movementX * Timer.getDelta();
+        cameraParent.rotation.x += e.movementY * Timer.getDelta() * -1;
+    }
+});
+
+window.addEventListener('mouseup', () => {
+    canvas.style.cursor = "default";
+});
+
+render.domElement.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+});
+
+window.addEventListener('resize', () => {
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+
+    render.setSize(canvas.clientWidth, canvas.clientHeight);
+    render.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+})
